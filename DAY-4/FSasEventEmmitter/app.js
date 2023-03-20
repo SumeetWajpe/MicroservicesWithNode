ebd358c95aa2082d471a3d96bfdf72d0647f221f
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-03-20 10:40:15 +0530

    ✨ using pipe()

--- DAY-4/FSasEventEmmitter/app.js
+++ DAY-4/FSasEventEmmitter/app.js
@@ -5,15 +5,16 @@ const fs = require("fs");
 const readableStream = fs.createReadStream("Input.txt");
 const writableStream = fs.createWriteStream("Output.txt");
 
-let dataFromFile = "";
-readableStream.on("data", chunk => {
-  console.log(
-    "\n\r>>>>>>>>>>>>>>>>>Chunk Received !>>>>>>>>>>>>>>>>>>>>>>>\n\r",
-  );
-  dataFromFile += chunk.toString();
-});
+// let dataFromFile = "";
+// readableStream.on("data", chunk => {
+//   console.log(
+//     "\n\r>>>>>>>>>>>>>>>>>Chunk Received !>>>>>>>>>>>>>>>>>>>>>>>\n\r",
+//   );
+//   dataFromFile += chunk.toString();
+// });
 
-readableStream.on("end", () => {
-  writableStream.write(dataFromFile);
-  writableStream.end();
-});
+// readableStream.on("end", () => {
+//   writableStream.write(dataFromFile);
+//   writableStream.end();
+// });
+readableStream.pipe(writableStream);
