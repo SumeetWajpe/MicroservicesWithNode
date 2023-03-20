02f715a981366a1eaa288b1ad8763cc6f2e6c694
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-03-20 13:23:01 +0530

    ✨ Adding Endpoints to Express

--- DAY-4/ExpressBasics/HelloWorldExpress/express.js
+++ DAY-4/ExpressBasics/HelloWorldExpress/express.js
@@ -7,6 +7,22 @@ app.get("/", (req, res) => {
   //   res.json({ msg: "Hello Express !" });
   res.sendFile("Index.html", { root: __dirname });
 });
+app.get("/script.js", (req, res) => {
+  res.sendFile("script.js", { root: __dirname });
+});
+
+app.get("/products", (req, res) => {
+  let products = [
+    { id: 1, title: "MacBookPro", price: 250000 },
+    { id: 2, title: "MacBookAir", price: 200000 },
+  ];
+  res.json(products);
+});
+
+// * - should be placed at the last after all endpoints are defined !
+// app.use((req, res) => {
+//   res.status(404).send("Resource not found !");
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
