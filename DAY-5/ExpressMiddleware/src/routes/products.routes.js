5beb6fd0821c2d7a60346eef9cf6c4787eccefb8
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-03-22 10:48:24 +0530

    ✨ ConsumeProductsInUI

--- DAY-5/ExpressMiddleware/src/routes/products.routes.js
+++ DAY-5/ExpressMiddleware/src/routes/products.routes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   let products = [
-    { id: 1, title: "MacBookPro", price: 250000 },
-    { id: 2, title: "MacBookAir", price: 200000 },
+    { id: 1, title: "MacBook Pro", price: 250000 },
+    { id: 2, title: "MacBook Air", price: 200000 },
+    { id: 3, title: "Nike Shoes", price: 2000 },
   ];
   res.json(products);
 });
--- DAY-5/ExpressMiddleware/src/static/script.js
+++ DAY-5/ExpressMiddleware/src/static/script.js
@@ -1,1 +1,27 @@
-console.log("Script Loaded !");
+// fetch api -> /products & display all product titles
+
+async function fetchAllProducts() {
+  try {
+    let response = await fetch("http://localhost:3000/products");
+    if (response.ok) {
+      let products = await response.json();
+      DisplayProducts(products);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function DisplayProducts(products) {
+  let listOfProducts = document.querySelector(".listofproducts");
+  for (const product of products) {
+    let newProductItem = document.createElement("li");
+    newProductItem.innerText = product.title;
+    newProductItem.style.width = "200px";
+
+    newProductItem.className = "list-group-item";
+    listOfProducts.appendChild(newProductItem);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", fetchAllProducts);
