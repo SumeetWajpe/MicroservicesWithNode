14dab90aac0addf706ef4974aaf53d20a620a292
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-03-27 10:39:14 +0530

    ✨ 📝 onlinetraining_atlas

---
+++ DAY-7/ConnectingWithMongoDbAtlas/onlinetraining/app.js
@@ -0,0 +1,58 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
+
+var coursesRouter = require("./routes/courses");
+
+var app = express();
+
+// mongoose.connect(
+//   "mongodb+srv://sumeetwajpe:vNwbqlBd7P5omI57@cluster0.xd8xtse.mongodb.net/trainingdb_atlas",
+// );
+// mongoose.connection.once("open", () => {
+//   console.log("Connected to Online Training DB - Atlas !");
+// });
+
+mongoose.connect(
+  "mongodb+srv://sumeetwajpe:vNwbqlBd7P5omI57@cluster0.xd8xtse.mongodb.net/trainingdb_atlas",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+);
+mongoose.connection.on("open", () => {
+  console.log("Connected to Online Training DB - Atlas !");
+});
+
+mongoose.connection.on("error", err => {
+  console.log(err);
+});
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", coursesRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ DAY-7/ConnectingWithMongoDbAtlas/onlinetraining/routes/courses.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+let listofcourses = require("../models/course.model");
+const fs = require("fs");
+const path = require("path");
+
+router.get("/", (req, res) => {
+  res.render("courses", { title: "List Of Courses", listofcourses });
+});
+
+router.get("/coursedetails/:id", (req, res) => {
+  let courseId = +req.params.id;
+  let course = listofcourses.find(course => course.id === courseId);
+  res.render("coursedetails", { course });
+});
+
+router.route("/video/:id").get((req, res) => {
+  let courseId = +req.params.id;
+  let course = listofcourses.find(course => course.id === courseId);
+  let videoPath = course.introVideo;
+
+  console.log(videoPath);
+  const range = req.headers.range;
+  const videoSize = fs.statSync(videoPath).size;
+  console.log(videoSize);
+
+  const CHUNK_SIZE = 10 ** 6; // 1 MB
+  // range
+  const start = Number(range.replace(/\D/g, ""));
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+  // headers
+  const contentLength = end - start + 1;
+  const headers = {
+    "content-Range": `bytes ${start}-${end}/${videoSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Type": "video/mp4",
+    "Content-Length": contentLength,
+  };
+  const videoStream = fs.createReadStream(
+    videoPath,
+    {
+      start,
+      end,
+    },
+  );
+  res.writeHead(206, headers);
+
+  videoStream.pipe(res); // stream the video as response to client
+});
+// router.post("/newcourse", (req, res) => {
+//   let newCourse = req.body;
+//   courses.push(newCourse);
+//   res.json({ msg: "success" });
+// });
+
+// router.delete("/:id", (req, res) => {
+//   // delete logic
+//   let theCourseId = +req.params.id;
+//   courses = courses.filter(course => course.id != theCourseId); // database
+//   res.json({ msg: "success" });
+// });
+
+module.exports = router;
