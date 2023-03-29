73867638c113672491c938163449f2480d98e867
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-03-29 12:40:08 +0530

    ✨ HelloTypescript

---
+++ DAY-8/Typescript/hellotypescript.js
@@ -0,0 +1,9 @@
+// console.log("Hello Typescript !");
+var x = "10"; // Type inference
+console.log(typeof x);
+//x = "Hello!"; // Type - safe
+// Type annotations
+var str;
+// str = 100;
+var z;
+console.log(z);
---
+++ DAY-8/Typescript/hellotypescript.ts
@@ -0,0 +1,12 @@
+// console.log("Hello Typescript !");
+
+var x = "10"; // Type inference
+console.log(typeof x);
+//x = "Hello!"; // Type - safe
+
+// Type annotations
+var str: string;
+// str = 100;
+
+var z;
+console.log(z);
