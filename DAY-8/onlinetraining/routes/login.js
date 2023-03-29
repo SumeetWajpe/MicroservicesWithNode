e57ac005f30bc20f4a8214e9db991038bf1b0ea4
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-03-29 10:37:35 +0530

    📝 Login Biz Logic - Creating Cookie,redirect to Courses

--- DAY-8/onlinetraining/routes/login.js
+++ DAY-8/onlinetraining/routes/login.js
@@ -1,7 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user.model");
+require("dotenv").config();
+var jwt = require("jsonwebtoken");
+
 router.get("/", (req, res) => {
   res.render("login");
+});
+
+router.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.render("error", { message: "User does not exist !" });
+  }
+  // sign -> generate the token !
+  let payload = { email };
+  jwt.sign(
+    payload,
+    process.env.JWT_SECRET,
+    { expiresIn: "2 Days" },
+    (err, token) => {
+      if (err) console.log(err);
+      else {
+        // create a cookie
+        res.cookie("jwt-token", token);
+        return res.redirect(302, "http://localhost:3000/courses");
+      }
+    },
+  );
 });
 
 module.exports = router;
