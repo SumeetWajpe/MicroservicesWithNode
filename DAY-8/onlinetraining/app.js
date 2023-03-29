303b930b566e5a70040a2d8c2f4de1eb90b5abee
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-03-29 10:12:58 +0530

    Added Login Router

--- DAY-8/onlinetraining/app.js
+++ DAY-8/onlinetraining/app.js
@@ -2,10 +2,12 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
+
 var logger = require("morgan");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+var loginRouter = require("./routes/login");
 var coursesRouter = require("./routes/courses");
 
 var app = express();
@@ -38,7 +40,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-
+app.use("/", loginRouter);
 app.use("/courses", coursesRouter);
 
 // catch 404 and forward to error handler
