f03b5623f1e5fe0dd9d96d98ba0b34d68eb885f8
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-04-05 12:28:27 +0530

    📝 Using Clusters

---
+++ DAY-10/NodePerformance/cluster.js
@@ -0,0 +1,35 @@
+const cluster = require("cluster");
+const http = require("http");
+
+const os = require("os");
+
+let numOfCores = os.cpus().length;
+console.log(numOfCores);
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+if (cluster.isPrimary) {
+  console.log(`Primary process  ${process.pid} running !`);
+  cluster.fork();
+  cluster.fork();
+} else {
+  console.log(`Worker process ${process.pid} running !`);
+
+  const server = http.createServer((req, res) => {
+    if (req.url == "/non-blocking") {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("Non Blocking Response !");
+    } else if (req.url == "/blocking") {
+      for (let index = 0; index < 6000000000; index++) {}
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("Blocking Response !");
+    }
+  });
+
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+}
