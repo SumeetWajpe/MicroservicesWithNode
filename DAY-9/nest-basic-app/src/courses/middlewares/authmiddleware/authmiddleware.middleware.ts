9a85b8cd7074b8337a61cc756787c302aeaf8fa3
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-04-05 10:26:31 +0530

    📝 Auth header - Token , thrwo Exceptions

--- DAY-9/nest-basic-app/src/courses/middlewares/authmiddleware/authmiddleware.middleware.ts
+++ DAY-9/nest-basic-app/src/courses/middlewares/authmiddleware/authmiddleware.middleware.ts
@@ -1,10 +1,30 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  ForbiddenException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class AuthmiddlewareMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     console.log('Within Auth Middleware !');
-    next();
+
+    console.log(req.headers.authorization);
+    const { authorization } = req.headers;
+    if (!authorization) {
+      // throw new HttpException(
+      //   'No Authorization Token Found !',
+      //   HttpStatus.FORBIDDEN,
+      // );
+
+      throw new ForbiddenException('No Authorization Token Found !');
+    } else if (authorization == 'jwt-token') {
+      next();
+    } else {
+      throw new HttpException('Invalid Token', HttpStatus.FORBIDDEN);
+    }
   }
 }
