52bb83dfd72ed39ef631728f7022e1af38e032b9
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-04-17 10:07:17 +0530

    📝 Added  isAuthenticatedMiddleware

---
+++ DAY-12/online-training-microservices-rabbitmq/online-training-microservices/isAuthenticatedMiddleware/index.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+const SECRET_KET = "SECRETKEYFORJWT";
+module.exports = async function isAuthenticated(req, res, next) {
+  // verify()
+  if (req.headers["authorization"]) {
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, SECRET_KET, (err, decodedToken) => {
+      if (err) res.status(500).json({ err: err.message });
+      console.log(decodedToken);
+    });
+    res.json({ msg: "success" });
+  } else {
+    return res.status(500).json({ message: "Token Not found !" });
+  }
+};
