c0756f6f54a75a2e806ebc439e35efc3fac11599
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-05-02 09:57:12 +0530

    📝 Updated Login & Sign Up Components - Styles

--- DAY-14/onlinetrainingapp-withdocker/client/src/components/login.component.jsx
+++ DAY-14/onlinetrainingapp-withdocker/client/src/components/login.component.jsx
@@ -6,50 +6,54 @@ function Login() {
   let [user, setUser] = useState({ email: "", password: "" });
   let navigate = useNavigate();
   return (
-    <div className="main">
-      <input type="checkbox" id="chk" aria-hidden="true" />
+    <div className="main-container">
+      <div className="main">
+        <input type="checkbox" id="chk" aria-hidden="true" />
 
-      <div className="signup">
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-            fetch("http://onlinetraining.com/auth/login", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                ...user,
-              }),
-            })
-              .then(res => res.json())
-              .then(msg => {
-                if (msg["token"]) {
-                  localStorage["jwt-token"] = msg["token"];
-                  navigate("/listofcourses");
-                }
-              });
-          }}
-        >
-          <label htmlFor="chk" aria-hidden="true">
-            Sign In
-          </label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            required=""
-            onInput={e => setUser({ ...user, email: e.target.value })}
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            required=""
-            onInput={e => setUser({ ...user, password: e.target.value })}
-          />
-          <button>Sign In</button>
-        </form>
+        <div className="signup">
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              fetch("http://onlinetraining.com/auth/login", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  ...user,
+                }),
+              })
+                .then(res => res.json())
+                .then(msg => {
+                  if (msg["token"]) {
+                    localStorage["jwt-token"] = msg["token"];
+                    navigate("/listofcourses");
+                  }
+                });
+            }}
+          >
+            <label htmlFor="chk" aria-hidden="true" className="label">
+              Sign In
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className="input"
+              required=""
+              onInput={e => setUser({ ...user, email: e.target.value })}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="input"
+              required=""
+              onInput={e => setUser({ ...user, password: e.target.value })}
+            />
+            <button className="button">Sign In</button>
+          </form>
+        </div>
       </div>
     </div>
   );
--- DAY-14/onlinetrainingapp-withdocker/client/src/components/signup.component.jsx
+++ DAY-14/onlinetrainingapp-withdocker/client/src/components/signup.component.jsx
@@ -7,61 +7,68 @@ function SignUp() {
   let navigate = useNavigate();
 
   return (
-    <div className="main">
-      <input type="checkbox" id="chk" aria-hidden="true" />
+    <div className="main-container">
+      <div className="main">
+        <input type="checkbox" id="chk" aria-hidden="true" className="label" />
 
-      <div className="signup">
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-            fetch("http://onlinetraining.com/auth/register", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                ...newuser,
-              }),
-            })
-              .then(res => {
-                if (res.ok) {
-                  return res;
-                }
+        <div className="signup">
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              fetch("http://onlinetraining.com/auth/register", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  ...newuser,
+                }),
               })
-              .then(addeduser => {
-                if (addeduser?.name) {
-                  // navigate("/");
-                  window.location.href = "/";
-                }
-              });
-          }}
-        >
-          <label for="chk" aria-hidden="true">
-            Sign up
-          </label>
+                .then(res => {
+                  if (res.ok) {
+                    return res;
+                  }
+                })
+                .then(addeduser => {
+                  if (addeduser?.name) {
+                    // navigate("/");
+                    window.location.href = "/";
+                  }
+                });
+            }}
+          >
+            <label for="chk" aria-hidden="true" className="label">
+              Sign up
+            </label>
 
-          <input
-            type="text"
-            name="txt"
-            placeholder="User name"
-            required=""
-            onInput={e => setNewUser({ ...newuser, name: e.target.value })}
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            required=""
-            onInput={e => setNewUser({ ...newuser, email: e.target.value })}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            required=""
-            onInput={e => setNewUser({ ...newuser, password: e.target.value })}
-          />
-          <button>Sign Up</button>
-        </form>
+            <input
+              type="text"
+              name="txt"
+              placeholder="User name"
+              required=""
+              className="input"
+              onInput={e => setNewUser({ ...newuser, name: e.target.value })}
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              required=""
+              className="input"
+              onInput={e => setNewUser({ ...newuser, email: e.target.value })}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              required=""
+              className="input"
+              onInput={e =>
+                setNewUser({ ...newuser, password: e.target.value })
+              }
+            />
+            <button>Sign Up</button>
+          </form>
+        </div>
       </div>
     </div>
   );
